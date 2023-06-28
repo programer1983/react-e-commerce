a4d1e7ebdb1187ce27ffba2f3a9135579e5adba0
programer1983 <programer972@gmail.com>
2023-06-28 10:54:31 +0300

    creating a routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <span>Home</span>
+  },
+  {
+    path: "/products/:id",
+    element: <span>Category</span>
+  },
+  {
+    path: "/product/:id",
+    element: <span>Product</span>
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-       Hello Dimon!
+       <RouterProvider router={router} />
     </div>
   );
 }
