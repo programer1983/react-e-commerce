d97925bc12f77bc15821a7c33376eed6aa28b614
programer1983 <programer972@gmail.com>
2023-06-29 16:21:56 +0300

    create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom"
-import './App.css';
+import './App.scss';
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
