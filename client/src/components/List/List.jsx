cee2ecb3698e3b9070078798d890e9ec676ba281
programer1983 <programer972@gmail.com>
2023-07-03 16:51:25 +0300

    creating a products  page by category

---
+++ client/src/components/List/List.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import "./List.scss"
+import Card from "./../../components/Card/Card"
+
+const List = ({catId, maxPrice, sort}) => {
+    const data = [
+        {
+            id: 1,
+            img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&tinysrgb&w=1600",
+            img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&tinysrgb&w=1600",
+            title: "Long Sleeve Graphic T-shirt",
+            isNew: true,
+            oldPrice: 19,
+            price: 12,
+        },
+        {
+            id: 2,
+            img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&tinysrgb&w=1600",
+            title: "Coat",
+            isNew: true,
+            oldPrice: 19,
+            price: 12,
+        },
+        {
+            id: 3,
+            img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&tinysrgb&w=1600",
+            title: "Skrit",
+            oldPrice: 19,
+            price: 12,
+        },
+        {
+            id: 4,
+            img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&tinysrgb&w=1600",
+            title: "Hat",
+            oldPrice: 19,
+            price: 12,
+        },
+     ]
+  
+     return (
+    <div className='list'>
+        {data?.map((item) => (
+            <Card key={item.id} item={item}/>
+        ))}
+    </div>
+  )
+}
+
+export default List
